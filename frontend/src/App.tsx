f64e2857c0ab8d0bf5507d5d4522ea3bc47186ec
gonago777 <hjjh5019@naver.com>
2026-01-15 17:22:34 +0900

    QnA FAQ로 수정 작업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import NoticeAlertPage from "./pages/NoticeAlertPage";
 import NoticeNewPage from "./pages/NoticeNewPage";
 import FileUploadPage from "./pages/FileUploadPage";
 import DraftPage from "./pages/DraftPage";
+import FaqPage from "./pages/FaqPage";
 
 function App() {
   return (
@@ -23,13 +24,19 @@ function App() {
         {/* 레이아웃 적용 페이지 */}
         <Route element={<Layout />}>
           <Route path="/" element={<MainPage />} />
+
           {/* 공고 */}
           <Route path="/notice" element={<NoticeAlertPage />} />
           <Route path="/notice/new" element={<NoticeNewPage />} />
+
           {/* 파일 업로드 */}
           <Route path="/upload" element={<FileUploadPage />} />
+
           {/* 초안 작성 */}
           <Route path="/draft" element={<DraftPage />} />
+
+          {/* FAQ */}
+          <Route path="/faq" element={<FaqPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
