63d8ee168ba7b666787c7770027365d0cc3b6364
SeongGunmo <ttingppong@naver.com>
2026-01-30 13:37:42 +0900

    로컬호스트 url 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "nonatomical-unmediaeval-sha.ngrok-free.dev",
-    ],
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
 });
 
