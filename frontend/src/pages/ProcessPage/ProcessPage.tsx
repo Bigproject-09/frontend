009afa13d48529c2150b9bf11f0ec604b8c17b83
SeongGunmo <ttingppong@naver.com>
2026-02-04 21:48:22 +0900

    메인페이지 진짜 수정. vite.config의 defineConfig에 proxy가 이중으로 작성되어 오류뜬 현상 수정 ProcessPage 에 useNavigate, useLocation 중복 작성 수정

--- frontend/src/pages/ProcessPage/ProcessPage.tsx
+++ frontend/src/pages/ProcessPage/ProcessPage.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
-import { useNavigate, useLocation } from "react-router-dom";
 import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
 import { STORAGE_KEY } from "../../common/constants";
 
