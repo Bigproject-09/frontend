a65ccc84754168d16011d70a28f94df4c60035ab
gonago777 <hjjh5019@naver.com>
2026-02-04 21:34:39 +0900

    모델링 연동 중

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
     allowedHosts: [
       "nonatomical-unmediaeval-sha.ngrok-free.dev",
     ],
@@ -15,17 +14,6 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-=======
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-        secure: false,
-      }
-    }
->>>>>>> frontend_jh
   },
 });
 
